8ada40860ad95675f9f53eb3e40ea9798f041506
kruckenberg <nkruckenberg@gmail.com>
2025-08-31 14:26:46 -0500

    update junit version; fix setup script

--- setup.sh
+++ setup.sh
@@ -23,6 +23,6 @@ else
 fi
 
 # Download JUnit Platform Console Standalone
-JUNIT_VERSION="1.13.3"
+JUNIT_VERSION="1.13.4"
 mkdir -p lib
-curl -L -o lib/junit-platform-console-standalone.jar execute https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/$JUNIT_VERSION/junit-platform-console-standalone-$JUNIT_VERSION.jar
+curl -L -o lib/junit-platform-console-standalone.jar https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/$JUNIT_VERSION/junit-platform-console-standalone-$JUNIT_VERSION.jar
