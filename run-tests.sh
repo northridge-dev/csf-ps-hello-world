41da03d6e1ed94fb6590f309e7887f03a81f3ac0
kruckenberg <nkruckenberg@gmail.com>
2025-08-31 14:55:58 -0500

    exclude unused junit engines

--- run-tests.sh
+++ run-tests.sh
@@ -10,4 +10,6 @@ echo "Running tests..."
 java -jar lib/junit-platform-console-standalone.jar execute \
   --class-path bin \
   --scan-class-path \
+  --exclude-engine=junit-vintage \
+  --exclude-engine=junit-platform-suite \
   --reports-dir test-results
